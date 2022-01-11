441e79d3e9eaaaae2f0b06073a357b2f9158e3fb
Dongji Yang <dongji.yang@outlook.com>
2022-01-10 18:30:15 -0600

    init

---
+++ pages/dataTest.js
@@ -0,0 +1,28 @@
+import {getPoliticians} from '../lib/civic'
+
+export default function dataTestPage({data}){
+    return(
+        <div>
+            <h1>Little Shit</h1>
+            <ul>
+                {data.elections.map((one) => (
+                    <li>
+                        <h2>{one.id}</h2>
+                        <p>{one.name}</p>
+                        <p>{one.electionDay}</p>
+                        <p>{one.ocdDivisionId}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export async function getStaticProps(context){
+    const data = await getPoliticians()
+
+    return {
+        props: { data },
+    }
+    
+}
