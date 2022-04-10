61c8ed5f2428f808b88c86738f954fc187afe31a
root <root@ip-172-31-27-92.us-east-2.compute.internal>
2022-04-10 21:04:15 +0000

    rob added stability and start/stop bash scripts

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
-import {SWRConfig} from 'swr'
+import { SWRConfig } from 'swr'
 import fetchJson from '../lib/fetchJson'
 
+
+
 function MyApp({ Component, pageProps }) {
-  return(
-    <SWRConfig 
+  return (
+    <SWRConfig
       value={{
         fetcher: fetchJson,
         onError: (err) => {
@@ -16,5 +18,19 @@ function MyApp({ Component, pageProps }) {
     </SWRConfig>
   )
 }
+
+process.env.SERVER_PID = process.pid;
+console.error("[INFO] PID: " + process.pid);
+
+// httpsServer.listen(PORT, () => {
+//   console.log("HTTPS Server running on port 3000");
+// });
+
+
+// process.on("SIGTERM", (stream) => {
+//   httpsServer.close();
+//   console.log("HTTPS Server closed port 3000 gracefully.");
+//   process.exit(0);
+// });
 
 export default MyApp
---
+++ pollyServiceHealthTool.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+#POLLYWORLD=/usr/local/booyahLab/polly-yang
+
+#PATH=/usr/local/sbin:/iusr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/booyahLab/polly-yang:/snap/bin
+
+PLOG=pollynode.log
+PERRLOG=pollynode-error.log
+
+errorlog=pollynode-error.log;
+
+zero=0;
+
+#b=`ps -ef | grep yarn | grep -v grep | wc -l`;
+cd $POLLYWORLD
+x=`cat $errorlog | grep "PID" | tail --lines=1 | cut -d : -f 2`;
+echo "POLLY SERVER PROCESS ID: $x"
+
+#if [[ $b -ne $zero ]];
+b=`ps -ef | grep yarn | grep -v grep | wc -l`;
+if [ $b -eq 1 ]
+	then
+	echo "POLLYWORLD IS ALIVE AND WELL 🤠 🐘 🦅 🐐 🌎 ";
+	sleep 2 -s
+else
+	cd $POLLYWORLD
+	#echo "made it to pollyworld"
+	sleep 2 -s
+      nohup /usr/local/booyahLab/polly-yang/pollystartup.sh
+	echo "
+	**************************************************************************
+	The polly server has stopped communicating. Executing startup script 🚀 🚀 
+	**************************************************************************
+	" 
+	sleep 2 -s
+  # tail -f pollyServHealthCheck.log
+	#echo "pollyworld is dead";
+	sleep 2 -s
+	#echo "pollyworld is immortal. The platform is booting."
+fi
+
+#cd $POLLYWORLD
+#q=`service mongod status`;
+#echo "DATABASE HEALTH CHECK: $q"
+
