b46876f852f1bfa676c458ab12afb8219d5f580b
Ubuntu <ubuntu@ip-172-31-27-92.us-east-2.compute.internal>
2022-04-17 22:00:00 +0000

    removed comments from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   eslint: {
-<<<<<<< HEAD
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-
-=======
-    ignoreDuringBuilds: true,
-  },
->>>>>>> b769831217a724e970237e92184618076a851fe9
 }
