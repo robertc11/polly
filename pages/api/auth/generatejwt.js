7ac26fc13d4fec310168036974ed531690b8bf72
Dongji Yang <dongji.yang@outlook.com>
2022-06-26 10:31:06 -0500

    Revert "fixed typos"
    This reverts commit 234f4decc6297a8a767c1168fb1e65784a7e8093.

--- pages/api/auth/generatejwt.js
+++ pages/api/auth/generatejwt.js
@@ -3,6 +3,7 @@ import { sign } from 'jsonwebtoken'
 export default async function handler(req, res){
     if(req.method === "GET"){
         const payload = {
+            data: "Kellys feet are delicious and I want to lick them",
             uid: "622ba77a1e71f718701db001"
         }
         const jwt = sign(payload, process.env.SECRET, {
